91c23f9cc4267ab4b4fe06d73ec7f2b144d1567e
Aditi Lamkhede <aditi.lamkhede@gmail.com>
2020-02-15 23:33:22 -0800

    added contact form, about form and related files

---
+++ Personal Blog Website/scripts/main.js
@@ -0,0 +1,7 @@
+function OpenForm() {
+  document.getElementById("popupForm").style.display = "block";
+}
+
+function CloseForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
